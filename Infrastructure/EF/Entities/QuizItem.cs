15c7e228beca00b55b9f0f5465d5b012406beb56
Remigiusz <remigiusz.krupa@microsoft.wsei.edu.pl>
2024-04-10 11:23:22 +0200

    lab-06

---
+++ Infrastructure/EF/Entities/QuizItem.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.EF.Entities
+{
+    public class QuizItem : IIdentity<int>
+    {
+        public int Id { get; set; }
+        public string Question { get; }
+        public List<string> IncorrectAnswers { get; }    // problem!
+        public string CorrectAnswer { get; }
+
+        public QuizItem(int id, string question, List<string> incorrectAnswers, string correctAnswer)
+        {
+            Id = id;
+            Question = question;
+            IncorrectAnswers = incorrectAnswers;
+            CorrectAnswer = correctAnswer;
+        }
+    }
+}
